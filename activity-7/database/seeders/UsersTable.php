8aacde493859cfc3fe466080c9bf009e70d01c79
josemario234 <AL02963295@tecmilenio.mx>
2026-03-07 20:25:12 -0700

    Creating a seeders for users

--- activity-7/database/seeders/UsersTable.php
+++ activity-7/database/seeders/UsersTable.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UsersTable extends Seeder
 {
@@ -12,6 +13,23 @@ class UsersTable extends Seeder
      */
     public function run(): void
     {
-        //
+        DB::table('users')->insert([
+            'Username' => 'Admon',
+            'Email' => ' admon@robotics.com',
+            'Password' => 'Adm@2022',
+            'Role' => 'Administrative'
+        ],
+        [
+            'Username' => 'Tecmilenio',
+            'Email' => ' tecmilenio@robotics.com',
+            'Password' => 'Adm@2022',
+            'Role' => 'Teacher'
+        ],
+        [
+            'Username' => 'Student',
+            'Email' => ' student@robotics.com',
+            'Password' => 'Adm@2022',
+            'Role' => 'Student'
+        ]);
     }
 }
